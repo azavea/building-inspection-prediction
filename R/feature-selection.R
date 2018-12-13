4df54fc5acba3fc86b572d2cbdaf3b32fc884aeb
Simon Kassel <skassel@azavea.com>
2018-12-13 13:08:23 -0500

    fix feature selection code

--- R/feature-selection.R
+++ R/feature-selection.R
@@ -33,10 +33,10 @@ dd <- ds %>%
 # partition dataset for boruta
 set.seed(12345)
 inTrain <- createDataPartition(dd$o.failed.n, p = 0.6, list = FALSE)
-boruta.train.set <- dd[inTrain, -c(1,2)]
+boruta.train.set <- dd[inTrain, -2]
 
 # run iterative random forrest variable importance test
-boruta.train <- Boruta(o.failed ~ ., data = boruta.train.set, doTrace = 2, maxRuns = 50)
+boruta.train <- Boruta(o.failed.n ~ ., data = boruta.train.set, doTrace = 2, maxRuns = 50)
 # get final decision
 fd <- boruta.train$finalDecision
 
